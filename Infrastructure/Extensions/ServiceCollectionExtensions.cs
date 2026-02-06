578654cfc5a09f0db245d93199d99b1f21da781c
Morteza Nourani <morteza.nourani@gmail.com>
2026-02-06 16:36:08 +0330

    Add City Version Application Project

---
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Components.Account;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection Services,
+        string connectionString)
+    {
+        Services.AddDbContextFactory<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        //Services.AddScoped<IdentityUserAccessor>();
+        //Services.AddScoped<IdentityRedirectManager>();
+        //Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        return Services;
+    }
+}
