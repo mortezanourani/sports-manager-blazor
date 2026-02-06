38778908daade7e2f69e3c32f28a1638d9d16bb2
Morteza Nourani <morteza.nourani@gmail.com>
2026-02-06 19:42:58 +0330

    Add Identity Service Extension

---
+++ Infrastructure/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureIdentity(
+        this IServiceCollection Services,
+        string connectionString)
+    {
+        Services.AddDbContextFactory<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+            options.SignIn.RequireConfirmedAccount = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireDigit = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders()
+        .AddSignInManager();
+
+        return Services;
+    }
+}
