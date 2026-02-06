f30381eca5cf0e23e4ea0a13d1ce7978ca4eaa34
Morteza Nourani <morteza.nourani@gmail.com>
2026-02-06 22:54:31 +0330

    Add Province Version Application Project

---
+++ ProvinceApp/Program.cs
@@ -0,0 +1,39 @@
+using ProvinceApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddInfrastructure(
+    builder.Configuration.GetConnectionString("ProvinceConnection"));
+
+builder.Services.AddInfrastructureIdentity(
+    builder.Configuration.GetConnectionString("ProvinceConnection"));
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
