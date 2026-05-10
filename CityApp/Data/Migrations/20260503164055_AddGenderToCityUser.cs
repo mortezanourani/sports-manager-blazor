8e1de856e5a3ad0ed92a7dcbf5507dcfafcae49b
Morteza Nourani <morteza.nourani@gmail.com>
2026-05-10 17:02:59 +0330

    Add Accounts Management Panel
    - Accounts management pages added to administration panel.

---
+++ CityApp/Data/Migrations/20260503164055_AddGenderToCityUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderToCityUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenderId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_GenderId",
+                table: "AspNetUsers",
+                column: "GenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Genders_GenderId",
+                table: "AspNetUsers",
+                column: "GenderId",
+                principalTable: "Genders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Genders_GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GenderId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ CityApp/Data/Migrations/20260505071105_AddDepartmentModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NormalizedName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersianName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
