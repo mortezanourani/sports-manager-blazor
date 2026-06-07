6ecc1b19b5bd08c7491df7443ee0f6e17c827ab3
Morteza Nourani <morteza.nourani@gmail.com>
2026-06-08 01:05:24 +0330

    Refactor Federation Presidents Model

--- CityApp/Models/LocalFederationPresident.cs
+++ CityApp/Models/LocalFederationPresident.cs
@@ -7,26 +7,6 @@ namespace CityApp.Models;
 
 public class LocalFederationPresident : BaseFederationPresident
 {
-    public string? Name { get; set; } = null!;
-
-    public string? SeenCode { get; set; } = null!;
-
-    public string? BirthDate { get; set; } = null!;
-
-    public string? Phone { get; set; }
-
-    public string? EducationalQualification { get; set; }
-
-    public string? EducationalMajor { get; set; }
-
-    public string? AppointmentOrder { get; set; }
-
-    public string? AppointmentDate { get; set; }
-
-    public string? TermEnd { get; set; }
-
-    public bool IsPresident { get; set; }
-
     public string? PresidentId { get; set; }
     public virtual CityUser President { get; set; } = null!;
 
--- ProvinceApp/Models/LocalFederationPresident.cs
+++ ProvinceApp/Models/LocalFederationPresident.cs
@@ -6,6 +6,18 @@ namespace ProvinceApp.Models;
 
 public class LocalFederationPresident : BaseFederationPresident
 {
+    public string Name { get; set; } = null!;
+
+    public string SeenCode { get; set; } = null!;
+
+    public string BirthDate { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string? EducationalQualification { get; set; }
+
+    public string? EducationalMajor { get; set; }
+
     public Guid FederationId { get; set; }
     public virtual LocalFederation Federation { get; set; } = null!;
 }
